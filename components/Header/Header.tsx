3c437ca81d8d38eb0123bc0fa514bcb88bcc1133
Vlad Ivanyuk <vl.ivanyuk1994@gmail.com>
2024-05-23 18:57:48 +1000

    [+] header bg

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -6,23 +6,25 @@ import { classNames } from '@/utils/classNames/classNames';
 export const Header = () => {
   return (
     <header className={cls.header}>
-      <section className={classNames(cls.headerNavWrapper, {}, ['container'])}>
-        <Image src={Logo} alt='Vercel Logo' className={cls.vercelLogo} />
-        <nav className=''>
-          <ul className={cls.nav}>
-            <li>Главная</li>
-            <li>О компании</li>
-            <li>Услуги</li>
-            <li>Расписание</li>
-            <li>Цены</li>
-            <li>Отслеживание</li>
-            <li>Контакты</li>
-          </ul>
-        </nav>
-        <select name='lang' id='lang'>
-          <option value='ru'>Русский</option>
-          <option value='en'>English</option>
-        </select>
+      <section className={cls.headerWrapper}>
+        <div className={classNames(cls.headerNavWrapper, {}, ['container'])}>
+          <Image src={Logo} alt='Vercel Logo' className={cls.logo} />
+          <nav className={cls.nav}>
+            <ul className={cls.navList}>
+              <li>Главная</li>
+              <li>О компании</li>
+              <li>Услуги</li>
+              <li>Расписание</li>
+              <li>Цены</li>
+              <li>Отслеживание</li>
+              <li>Контакты</li>
+            </ul>
+          </nav>
+          <select name='lang' id='lang'>
+            <option value='ru'>Русский</option>
+            <option value='en'>English</option>
+          </select>
+        </div>
       </section>
       <section className={classNames('', {}, ['container'])}>
         МОРСКАЯ ПЕРЕВОЗКА ГРУЗОВ
