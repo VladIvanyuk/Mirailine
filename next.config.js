a5f9e7b030ab8edd0ab2e6fb7c91972b5df64cd9
Vlad Ivanyuk <vl.ivanyuk1994@gmail.com>
2024-05-27 16:22:54 +1000

    [~] next config

--- next.config.js
+++ next.config.js
@@ -3,6 +3,9 @@ const createNextIntlPlugin = require('next-intl/plugin');
 const withNextIntl = createNextIntlPlugin();
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  //   output: 'export',
+  //   images: { unoptimized: true },
+};
 
 module.exports = withNextIntl(nextConfig);
