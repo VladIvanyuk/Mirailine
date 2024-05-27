f1b9071ef4ac926bf8d30ad5087d7ecd873c1038
Vlad Ivanyuk <vl.ivanyuk1994@gmail.com>
2024-05-27 17:48:03 +1000

    [+] locales configs

--- config.ts
+++ config.ts
@@ -1,11 +1,6 @@
 import { Pathnames } from 'next-intl/navigation';
 
-export const port = process.env.PORT || 3000;
-export const host = process.env.VERCEL_URL
-  ? `https://${process.env.VERCEL_URL}`
-  : `http://localhost:${port}`;
-
-export const defaultLocale = 'en' as const;
+export const defaultLocale = 'ru' as const;
 export const locales = ['en', 'ru'] as const;
 
 export const pathnames = {
@@ -20,5 +15,3 @@ export const pathnames = {
 
 // Use the default: `always`
 export const localePrefix = undefined;
-
-export type AppPathnames = keyof typeof pathnames;
