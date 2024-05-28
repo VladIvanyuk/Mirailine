c3b6657fbbdff310b1146a1dd9d3419e1d562ec9
Vlad Ivanyuk <vl.ivanyuk1994@gmail.com>
2024-05-28 11:38:10 +1000

    [~] fonts

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -4,11 +4,12 @@ import Logo from '@/public/logo.png';
 import { classNames } from '@/utils/classNames/classNames';
 import { IHeaderProps } from './types';
 import Link from 'next/link';
-import { useTranslations } from 'next-intl';
+import { useLocale, useTranslations } from 'next-intl';
 import { LocaleSwitcher } from '../LocaleSwitcher/LocaleSwitcher';
 
 export const Header = ({ view }: IHeaderProps) => {
   const t = useTranslations('Header');
+  const activeLocale = useLocale();
   return (
     <header className={classNames(cls.header, {}, [cls[view]])}>
       <section className={cls.headerWrapper}>
@@ -22,37 +23,49 @@ export const Header = ({ view }: IHeaderProps) => {
           <nav className={cls.nav}>
             <ul className={cls.navList}>
               <li>
-                <Link className={cls.navLink} href="/">
+                <Link className={cls.navLink} href={`/${activeLocale}`}>
                   {t('Home')}
                 </Link>
               </li>
               <li>
-                <Link className={cls.navLink} href="/about">
+                <Link className={cls.navLink} href={`/${activeLocale}/about`}>
                   {t('About')}
                 </Link>
               </li>
               <li>
-                <Link className={cls.navLink} href="/services">
+                <Link
+                  className={cls.navLink}
+                  href={`/${activeLocale}/services`}
+                >
                   {t('Services')}
                 </Link>
               </li>
               <li>
-                <Link className={cls.navLink} href="/schedule">
+                <Link
+                  className={cls.navLink}
+                  href={`/${activeLocale}/schedule`}
+                >
                   {t('Schedule')}
                 </Link>
               </li>
               <li>
-                <Link className={cls.navLink} href="/prices">
+                <Link className={cls.navLink} href={`/${activeLocale}/prices`}>
                   {t('Prices')}
                 </Link>
               </li>
               <li>
-                <Link className={cls.navLink} href="/tracking">
+                <Link
+                  className={cls.navLink}
+                  href={`/${activeLocale}/tracking`}
+                >
                   {t('Tracking')}
                 </Link>
               </li>
               <li>
-                <Link className={cls.navLink} href="/contacts">
+                <Link
+                  className={cls.navLink}
+                  href={`/${activeLocale}/contacts`}
+                >
                   {t('Contacts')}
                 </Link>
               </li>
