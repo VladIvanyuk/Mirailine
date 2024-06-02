555281bf2de682d6aa34df9dc2fe14a34c23f77e
Vlad Ivanyuk <vl.ivanyuk1994@gmail.com>
2024-06-02 22:05:15 +1000

    [+] services

--- app/[locale]/services/page.tsx
+++ app/[locale]/services/page.tsx
@@ -1,13 +1,33 @@
 import { Header } from '@/components/Header/Header';
 import { HeaderView } from '@/components/Header/types';
 import cls from './page.module.scss';
+import { SectionLayout } from '@/components/SectionLayout/SectionLayout';
+import { LineVariants, Title } from '@/components/Title/Title';
+import TitleServicesDecor from '@/public/images/text/services.png';
+import { Description } from '@/components/Description/Description';
 
 export default function Services() {
   return (
     <>
       <Header view={HeaderView.Services} />
-      <main className={cls.main}>
-        <section className="container">SERVICES PAGE</section>
+      <main>
+        <SectionLayout className="container">
+          <Title
+            decorText={TitleServicesDecor}
+            text="Services"
+            lineVariant={LineVariants.VARIANT_SIX}
+          />
+          <div>
+            <div>
+              <Description className={cls.servicesDescr}>
+                Осуществляем полный спектр услуг по доставке Вашего груза из
+                порта в порт. доверьтесь нашей команде профессионалов и
+                отслеживайте груз в реальном времени
+              </Description>
+            </div>
+            <div></div>
+          </div>
+        </SectionLayout>
       </main>
     </>
   );
