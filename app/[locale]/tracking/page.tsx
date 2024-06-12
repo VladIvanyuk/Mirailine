f197fe95c0addcc3622d73cd48a46796431a0d6c
Vlad Ivanyuk <vl.ivanyuk1994@gmail.com>
2024-06-12 16:06:22 +1000

    [+] add tracking title

--- app/[locale]/tracking/page.tsx
+++ app/[locale]/tracking/page.tsx
@@ -1,13 +1,24 @@
 import { Header } from '@/components/Header/Header';
 import { HeaderView } from '@/components/Header/types';
 import cls from './page.module.scss';
+import { SectionLayout } from '@/components/SectionLayout/SectionLayout';
+import { LineVariants, Title } from '@/components/Title/Title';
+import { useTranslations } from 'next-intl';
+import TitleTrackingDecor from '@/public/images/text/tracking.png';
 
 export default function Tracking() {
+  const t = useTranslations('Tracking');
   return (
     <>
       <Header view={HeaderView.Tracking} />
-      <main className={cls.main}>
-        <section className="container">Tracking PAGE</section>
+      <main>
+        <SectionLayout>
+          <Title
+            text={t('Tracking')}
+            lineVariant={LineVariants.VARIANT_THREE}
+            decorText={TitleTrackingDecor}
+          />
+        </SectionLayout>
       </main>
     </>
   );
