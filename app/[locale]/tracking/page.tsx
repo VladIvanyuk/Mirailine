cdcf06bc670ae3551fd0b4e7f28deca953c2473e
Vlad Ivanyuk <vl.ivanyuk1994@gmail.com>
2024-06-18 18:53:54 +1000

    [+] tracking page

--- app/[locale]/tracking/page.tsx
+++ app/[locale]/tracking/page.tsx
@@ -7,8 +7,35 @@ import { useTranslations } from 'next-intl';
 import TitleTrackingDecor from '@/public/images/text/tracking.png';
 import { TrackingSearch } from '@/components/TrackingSearch/TrackingSearch';
 
+export interface ITranslationsTypes {
+  placeholder: string;
+  button: string;
+  title: string;
+  frameNumber: string;
+  frameName: string;
+  sailingDate: string;
+  status: string;
+  waiting: string;
+  onTheWay: string;
+  download: string;
+}
+
 export default function Tracking() {
   const t = useTranslations('Tracking');
+
+  const searchComponentTranslations: ITranslationsTypes = {
+    placeholder: t('TrackingSearchPlaceholder'),
+    button: t('TrackingSearchButton'),
+    title: t('TrackingSearch'),
+    frameNumber: t('FrameNumber'),
+    frameName: t('FrameName'),
+    sailingDate: t('SailingDate'),
+    status: t('Status'),
+    waiting: t('Waiting'),
+    onTheWay: t('OnTheWay'),
+    download: t('Download'),
+  };
+
   return (
     <>
       <Header view={HeaderView.Tracking} />
@@ -19,11 +46,7 @@ export default function Tracking() {
             lineVariant={LineVariants.VARIANT_THREE}
             decorText={TitleTrackingDecor}
           />
-          <TrackingSearch
-            placeholder={t('TrackingSearchPlaceholder')}
-            button={t('TrackingSearchButton')}
-            title={t('TrackingSearch')}
-          />
+          <TrackingSearch translations={searchComponentTranslations} />
         </SectionLayout>
       </main>
     </>
--- components/TrackingSearch/TrackingSearch.tsx
+++ components/TrackingSearch/TrackingSearch.tsx
@@ -12,12 +12,11 @@ import 'swiper/css/pagination';
 import { Navigation, Pagination } from 'swiper/modules';
 import cls from './TrackingSearch.module.scss';
 import { handleZip } from '@/utils/imgDownloader/imgDownloader';
+import { ITranslationsTypes } from '@/app/[locale]/tracking/page';
 
 interface ITrackingSearchProps {
   className?: string;
-  title: string;
-  placeholder: string;
-  button: string;
+  translations: ITranslationsTypes;
 }
 
 export interface IFrameData {
@@ -35,9 +34,18 @@ const URL = 'https://www.vl1c.ru/mirayunfhttp/hs/post/product_json/VM4-082144';
 
 export const TrackingSearch = ({
   className,
-  title,
-  placeholder,
-  button,
+  translations: {
+    title,
+    placeholder,
+    button,
+    frameName,
+    frameNumber,
+    onTheWay,
+    sailingDate,
+    status,
+    waiting,
+    download,
+  },
 }: ITrackingSearchProps) => {
   const [frameData, setFrameData] = useState<IFrameData | null>(null);
   const [isShowCarousel, setIsShowCarousel] = useState(false);
@@ -59,6 +67,20 @@ export const TrackingSearch = ({
       setIsError(false);
     }
   };
+
+  const getFrameStatus = () => {
+    let flag;
+
+    if (frameData) {
+      const sailingDate = new Date(frameData.SailingOnOrAbout);
+      const nowDate = new Date();
+      flag = sailingDate > nowDate;
+    }
+
+    return flag;
+  };
+
+  const isFrameSailing = getFrameStatus();
 
   return (
     <div className={classNames(cls.trackingSearch, {}, [className])}>
@@ -88,7 +110,7 @@ export const TrackingSearch = ({
                     src={el.image}
                     width={1000}
                     height={650}
-                    alt="Картинка кузова"
+                    alt='Картинка кузова'
                   />
                 </SwiperSlide>
               ))}
@@ -99,7 +121,7 @@ export const TrackingSearch = ({
         {title}
       </Description>
       <div>
-        <input type="text" className={cls.input} placeholder={placeholder} />
+        <input type='text' className={cls.input} placeholder={placeholder} />
         <button onClick={searchFrame} className={cls.button}>
           {button}
         </button>
@@ -107,6 +129,30 @@ export const TrackingSearch = ({
       <div>
         {frameData && (
           <>
+            <ul className={cls.frameInfo}>
+              <li className={cls.frameInfoItem}>
+                <p className={cls.frameItemTitle}>{frameNumber}</p>
+                <p className={cls.frameItemValue}>{frameData.FlightNumber}</p>
+              </li>
+              <li className={cls.frameInfoItem}>
+                <p className={cls.frameItemTitle}>{frameName}</p>
+                <p className={cls.frameItemValue}>
+                  {frameData.NameOfTheVessel}
+                </p>
+              </li>
+              <li className={cls.frameInfoItem}>
+                <p className={cls.frameItemTitle}>{sailingDate}</p>
+                <p className={cls.frameItemValue}>
+                  {frameData.SailingOnOrAbout.substring(0, 10)}
+                </p>
+              </li>
+              <li className={cls.frameInfoItem}>
+                <p className={cls.frameItemTitle}>{status}</p>
+                <p className={cls.frameItemValue}>
+                  {isFrameSailing ? waiting : onTheWay}
+                </p>
+              </li>
+            </ul>
             <ul className={cls.imgsList}>
               {frameData.images.map((el, index) => (
                 <li
@@ -121,13 +167,16 @@ export const TrackingSearch = ({
                     src={el.image}
                     width={200}
                     height={130}
-                    alt="Картинка кузова"
+                    alt='Картинка кузова'
                   />
                 </li>
               ))}
             </ul>
-            <button onClick={() => handleZip(frameData.images)}>
-              DOWNLOAD
+            <button
+              className={cls.downloadBtn}
+              onClick={() => handleZip(frameData.images)}
+            >
+              {download}
             </button>
           </>
         )}
